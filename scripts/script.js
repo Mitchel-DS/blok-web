7934cac0adecb8de32d1482fbd9534b3856bab44
Yami-Matoi <90356313+Yami-Matoi@users.noreply.github.com>
2021-10-01 04:04:49 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 // JavaScript Document
+console.log("connection gelukt");
+
+var menubutton = document.querySelector(".menu-button");
+var cartbutton = document.querySelector(".cart-button");
+var navigation = document.querySelector(".navigation-menu-hidden");
+
+menubutton.addEventListener('click', dropMenu);
+cartbutton.addEventListener('click', dropCart);
+
+function dropMenu(){
+    navigation.classList.toggle("navigation-menu");
+}
