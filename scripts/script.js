81687ff982f6c583e7bd3cb73f962e357e5c842c
Yami-Matoi <90356313+Yami-Matoi@users.noreply.github.com>
2021-10-05 02:23:09 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,23 @@ console.log("connection gelukt");
 
 var menubutton = document.querySelector(".menu-button");
 var cartbutton = document.querySelector(".cart-button");
+var nav = document.querySelector(".navigation");
 var navigation = document.querySelector(".navigation-menu-hidden");
+var cart = document.querySelector(".navigation-cart-hidden");
 
 menubutton.addEventListener('click', dropMenu);
 cartbutton.addEventListener('click', dropCart);
 
 function dropMenu(){
-    navigation.classList.toggle("navigation-menu");
+    if(!cart.classList.contains(".navigation-cart")){
+        navigation.classList.toggle("navigation-menu");
+        cart.classList.remove("navigation-cart");
+    }
+}
+
+function dropCart(){
+    if(!navigation.classList.contains(".navigation-menu")){
+        cart.classList.toggle("navigation-cart");
+        navigation.classList.remove("navigation-menu");
+    }  
 }
